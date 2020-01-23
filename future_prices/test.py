c513a79bfab59f0e7a25bfd580fea810f5b6245f
edolev89 <edolev89@gmail.com>
2020-01-22 20:01:45 -0500

    temp

--- future_prices/test.py
+++ future_prices/test.py
@@ -1,5 +1,6 @@
 import torch
 import pandas as pd
+import numpy as np
 
 
 def test(model, predict_days, train_loader, test_loader, device, results_dir):
@@ -18,25 +19,30 @@ def test(model, predict_days, train_loader, test_loader, device, results_dir):
     with torch.no_grad():
         # predict existing days
         output = []
-        for batch_idx, (data, _) in enumerate(train_loader):
-            for k, v in data.items():
-                for k2, v2 in v.items():
-                    data[k][k2] = v2.float().to(device)
+        # for batch_idx, (data, _) in enumerate(train_loader):
+        #     for k, v in data.items():
+        #         for k2, v2 in v.items():
+        #             data[k][k2] = v2.float().to(device)
+        #
+        #     prediction = model(data)
+        #     output.append(prediction['next_prices'].detach().cpu().numpy())
+        #
+        #     if (batch_idx+1) % output_interval == 0 or (batch_idx+1) == num_batches:
+        #         print('predicting training day t+1 {}/{}...'.format(
+        #             batch_idx+1, num_batches))
+        #
+        #         print(len(columns))
+        #         print(len(output))
+        #         print(range(last_output_index, batch_idx+1))
+        #         print(output)
+        #         pd.DataFrame(output, columns=columns, index=range(last_output_index, batch_idx+1))\
+        #             .to_csv(output_path,
+        #                     header=False,
+        #                     mode='a')
+        #         last_output_index = batch_idx + 1
+        #         output = []
 
-            prediction = model(data)
-            output.append(prediction['next_prices'].detach().cpu().numpy())
-
-            if (batch_idx+1) % output_interval == 0 or (batch_idx+1) == num_batches:
-                print('predicting training day t+1 {}/{}...'.format(
-                    batch_idx+1, num_batches))
-
-                pd.DataFrame(output, columns=columns, index=range(last_output_index, batch_idx+1))\
-                    .to_csv(output_path,
-                            header=False,
-                            mode='a')
-                last_output_index = batch_idx + 1
-                output = []
-
+        output = []
         # predict new days
         for day in range(predict_days):
             for batch_idx, (data, _) in enumerate(test_loader):
@@ -46,6 +52,7 @@ def test(model, predict_days, train_loader, test_loader, device, results_dir):
 
                 prediction = model(data)
                 test_loader.add_day(prediction['next_prices'].cpu().numpy())
+                output.append(prediction['next_prices'].cpu().numpy())
 
                 if (day+1) % 5 == 0 or (day+1) == predict_days:
                     print('predicting new day {}/{}...'.format(day+1, predict_days))
@@ -54,7 +61,7 @@ def test(model, predict_days, train_loader, test_loader, device, results_dir):
             initial_data_shape, test_loader.futureprices.dataframe.shape))
 
     print('Saving results to "{}"...'.format(output_path))
-    output_df = test_loader.futureprices.dataframe.iloc[-predict_days:]
+    output_df = pd.DataFrame(np.array(output), columns=test_loader.futureprices.dataframe.columns)
     output_df.index = range(last_output_index, last_output_index+len(output_df))
     output_df.to_csv(output_path, header=False, mode='a')
 
